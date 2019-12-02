0fdf29fb200d7fe73dd7a18202fa1ef2cfc16447
Harsh Sharma <harsh@Harshs-MacBook-Pro.local>
2019-12-02 11:01:44 -0500

    Code complete. But needs to be debugged

--- ilqr.py
+++ ilqr.py
@@ -98,7 +98,7 @@ def simulate(env, x0, U):
     final_cost,_,_ = cost_final(deepcopy(env),X_new[-1])
     trajectory_cost+= final_cost 
     print("Trajectory cost ",trajectory_cost)
-    return trajectory_cost
+    return trajectory_cost,X_new
 
 # def get_f_x(env,x,U):
 #   tN = U.shape[0]
@@ -138,14 +138,19 @@ def calc_ilqr_input(env, sim_env, previous_action, tN=50, max_iter=1e6):
       The SEQUENCE of commands to execute. The size should be (tN, #parameters)
     """
 
+    CONVERGENCE_THRESHOLD = 0.01
+
     if(previous_action is None):
         prev_single_action = np.full((env.DOF, ), 1.0)
         previous_actions = np.tile(prev_single_action, (tN, 1))
 
+    env.reset()
+    present_state = np.copy(env.state)
+    old_trajectory_cost, X_traj = simulate(deepcopy(env),np.copy(present_state),previous_actions)
+
     for iteration_count in range(int(max_iter)):
       env.reset()
       present_state = np.copy(env.state)
-      old_trajectory_cost = simulate(deepcopy(env),np.copy(present_state),previous_actions)
 
       f_x = np.zeros((tN, present_state.shape[0], present_state.shape[0])) 
       f_u = np.zeros((tN, present_state.shape[0], env.DOF))
@@ -160,7 +165,7 @@ def calc_ilqr_input(env, sim_env, previous_action, tN=50, max_iter=1e6):
         A = approximate_A(sim_env,present_state,previous_actions[i])
         B = approximate_B(sim_env,present_state,previous_actions[i])
         f_x[i,:,:] = A
-        f_u[i,:,:] = B
+        f_u[i,:,:] = B                                         #See if this A and B is correct or if we need to follow the dt approach
         l[i], l_x[i,:], l_xx[i,:,:], l_u[i,:], l_uu[i,:,:], l_ux[i,:,:] = cost_inter(env,present_state,previous_actions[i])
         present_state,_,_,_ = env.step(previous_actions[i])   #See how to handle done situation here
 
@@ -176,10 +181,36 @@ def calc_ilqr_input(env, sim_env, previous_action, tN=50, max_iter=1e6):
 
         Q_x = l_x[t] + np.dot(f_x[t].T, V_x)
         Q_u = l_u[t] + np.dot(f_u[t].T, V_x)
+        Q_xx = l_xx[t] + np.dot(f_x[t].T, np.dot(V_xx, f_x[t])) 
+        Q_ux = l_ux[t] + np.dot(f_u[t].T, np.dot(V_xx, f_x[t]))
+        Q_uu = l_uu[t] + np.dot(f_u[t].T, np.dot(V_xx, f_u[t]))
+        Q_uu_inv = np.linalg.pinv(Q_uu)
 
+        k[t] = -np.dot(Q_uu_inv, Q_u)
+        K[t] = -np.dot(Q_uu_inv, Q_ux)
 
+        V_x = Q_x - np.dot(K[t].T, np.dot(Q_uu, k[t]))
+        V_xx = Q_xx - np.dot(K[t].T, np.dot(Q_uu, K[t]))
 
-      ipdb.set_trace()
+      new_actions = np.zeros((tN, env.DOF))
+      env.reset()
+      start_state = np.copy(env.state)
+      new_state = np.copy(env.state) 
+      for t in range(tN): 
+        new_actions[t] = previous_actions[t] + k[t] + np.dot(K[t], new_state - X_traj[t]) # 7b)
+        new_state,_,_,_ = env.step(new_actions[t]) 
+
+      env.reset()
+      new_trajectory_cost, Xnew  = simulate(deepcopy(env),np.copy(start_state),new_actions)
+      print("New Trajectory cost is: ",new_trajectory_cost)
+      previous_actions = new_actions
+
+      if(abs(old_trajectory_cost-new_trajectory_cost)<CONVERGENCE_THRESHOLD):
+        print("Convergence achived in iteration_count ",iteration_count)
+        return new_actions
+
+      old_trajectory_cost, X_traj = new_trajectory_cost,Xnew
+      # ipdb.set_trace()
 
 
     return np.zeros((50, 2))
